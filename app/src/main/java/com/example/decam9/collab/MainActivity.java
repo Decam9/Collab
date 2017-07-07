4302243e7f50dc1cbf83b1ec0004e1c15a390a87
decam9 <decam9@fb.com>
2017-07-07 11:06:31 -0700

    Initial commit

---
+++ app/src/main/java/com/example/decam9/collab/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.decam9.collab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
